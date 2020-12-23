499c2710e47e64da9f8375082af6e9fdc21ae6c6
billchen-818 <chenmingxing001@gmail.com>
2020-12-23 06:08:59 +0000

    添加fabric部署文件

---
+++ cryptogen_generate.sh
@@ -0,0 +1,8 @@
+#! /bin/bash 
+
+
+cryptogen generate --config=./organizations/cryptogen/crypto-config-org1.yaml --output="organizations"
+
+cryptogen generate --config=./organizations/cryptogen/crypto-config-org2.yaml --output="organizations"
+
+cryptogen generate --config=./organizations/cryptogen/crypto-config-orderer.yaml --output="organizations"
---
+++ stop_network.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+SYS_CHANNEL=system-channel COMPOSE_PROJECT_NAME=net docker-compose -f docker/docker-compose-test-net.yaml down
